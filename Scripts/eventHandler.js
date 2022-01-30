8b0de1e3194ec64c1837c494c7d155f32e7b6487
leila672 <lailatariq672@gmail.com>
2022-01-30 11:44:09 +0200

    event handle refactoring

--- Scripts/eventHandler.js
+++ Scripts/eventHandler.js
@@ -2,67 +2,67 @@
 let i = 0;
 let j = 0;
 
-document.getElementById(`item-${i}-${j}`).style.border = "3px solid black";
+function applyBorder(i, j) {
+    document.getElementById(`item-${i}-${j}`).style.border = "3px solid black";
+}
+
+function deleteBorder(i, j) {
+    document.getElementById(`item-${i}-${j}`).style.border = "";
+}
+
+applyBorder(i, j);
 
 document.onkeydown = function (event) {
     event = event || window.event;
     switch (event.keyCode) {
         case 37: //left
-            document.getElementById(`item-${i}-${j}`).style.border = "";
+            deleteBorder(i, j);
             j--;
             if (j == -1) j = 3;
-            document.getElementById(`item-${i}-${j}`).style.border = "3px solid black";
-
+            applyBorder(i, j);
             break;
         case 39: //right      
-            document.getElementById(`item-${i}-${j}`).style.border = "";
+            deleteBorder(i, j);
             j++;
             if (j == 4) j = 0;
-            document.getElementById(`item-${i}-${j}`).style.border = "3px solid black";
+            applyBorder(i, j);
             break;
         case 38: //up
-            document.getElementById(`item-${i}-${j}`).style.border = "";
+            deleteBorder(i, j);
             i--;
             if (i == -1) i = 3;
-            document.getElementById(`item-${i}-${j}`).style.border = "3px solid black";
+            applyBorder(i, j);
             break;
         case 40: //down 
-            document.getElementById(`item-${i}-${j}`).style.border = "";
+            deleteBorder(i, j);
             i++;
             if (i == 4) i = 0;
-            document.getElementById(`item-${i}-${j}`).style.border = "3px solid black";
+            applyBorder(i, j);
             break;
     }
 };
 
-
+function selectNum(i, j, id) {
+    let img = document.getElementById(`img-${i}-${j}`)
+    if (img.style.visibility == "hidden") {
+        document.getElementById(`item-${i}-${j}`).style.backgroundImage = `url(/images/Groups/1/${id}.png)`;
+        document.getElementById(`item-${i}-${j}`).style.backgroundSize = "cover";
+    }
+}
 document.onkeypress = function (event) {
     event = event || window.event;
-    let img1 = document.getElementById(`img-${i}-${j}`)
     switch (event.keyCode) {
         case 49: //number 1
-            if (img1.style.visibility == "hidden") {
-                document.getElementById(`item-${i}-${j}`).style.backgroundImage = "url('/images/Groups/1/1.png')";
-                document.getElementById(`item-${i}-${j}`).style.backgroundSize = "cover";
-            }
+            selectNum(i, j, 1);
             break;
         case 50: //number 2
-            if (img1.style.visibility == "hidden") {
-                document.getElementById(`item-${i}-${j}`).style.backgroundImage = "url('/images/Groups/1/2.png')";
-                document.getElementById(`item-${i}-${j}`).style.backgroundSize = "cover";
-            }
+            selectNum(i, j, 2);
             break;
         case 51: //number 3
-            if (img1.style.visibility == "hidden") {
-                document.getElementById(`item-${i}-${j}`).style.backgroundImage = "url('/images/Groups/1/3.png')";
-                document.getElementById(`item-${i}-${j}`).style.backgroundSize = "cover";
-            }
+            selectNum(i, j, 3);
             break;
         case 52: //number 4
-            if (img1.style.visibility == "hidden") {
-                document.getElementById(`item-${i}-${j}`).style.backgroundImage = "url('/images/Groups/1/4.png')";
-                document.getElementById(`item-${i}-${j}`).style.backgroundSize = "cover";
-            }
+            selectNum(i, j, 4);
             break;
 
     }
